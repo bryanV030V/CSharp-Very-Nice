d85ae11e692f897df5995acca34bb8f7650f4dbe
Bryan van 't Veld <61687852+bryanV030V@users.noreply.github.com>
2023-01-11 19:56:47 +0100

    Pingpong Doesnt work :-1:
    i dont know why ping pong no work

---
+++ YouNeedSomeMilk/YouNeedSomeMilk/PingPong CommendetOud/Ping pong no work.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace YouNeedSomeMilk.PingPong
+{
+    class Class1
+    {
+
+        //static void Main(string[] args)
+        //{
+        //    const int fieldLength = 50, fieldWidth = 15;
+        //    const char fieldTile = '#';
+        //    string line = string.Concat(Enumerable.Repeat(fieldTile, fieldLength));
+        //    const int racketLength = fieldWidth / 4;
+        //    const char racketTile = '|';
+
+        //    int leftRacketHeight = 0;
+        //    int rightRacketHeight = 0;
+
+        //    int ballX = fieldLength / 2;
+        //    int ballY = fieldWidth / 2;
+        //    const char ballTile = 'O';
+
+        //    bool isBallGoingDown = true;
+        //    bool isBallGoingRight = true;
+
+        //    int leftPlayerPoints = 0;
+        //    int rightPlayerPoints = 0;
+
+        //    int scoreboardX = fieldLength / 2 - 2;
+        //    int scoreboardY = fieldWidth + 1;
+
+        //    while (true)
+        //    {
+        //        Console.SetCursorPosition(0, 0);
+        //        Console.WriteLine(line);
+
+        //        Console.SetCursorPosition(0, fieldWidth);
+        //        Console.WriteLine(line);
+
+        //        for (int i = 0; i < racketLength; i++)
+        //        {
+        //            Console.SetCursorPosition(0, i + 1 + leftRacketHeight);
+        //            Console.WriteLine(racketTile);
+        //            Console.SetCursorPosition(fieldLength - 1, i + 1 + rightRacketHeight);
+        //            Console.WriteLine(racketTile);
+        //        }
+
+        //        while (!Console.KeyAvailable)
+        //        {
+        //            Console.SetCursorPosition(ballX, ballY);
+        //            Console.WriteLine(ballTile);
+        //            Thread.Sleep(300); //Adds a timer so that the players have time to react
+
+        //            Console.SetCursorPosition(ballX, ballY);
+        //            Console.WriteLine(" "); //Clears the previous position of the ball
+
+        //            //Update position of the ball
+        //            if (isBallGoingDown)
+        //            {
+        //                ballY++;
+        //            }
+        //            else
+        //            {
+        //                ballY--;
+        //            }
+        //            if (isBallGoingRight)
+        //            {
+        //                ballX++;
+        //            }
+        //            else
+        //            {
+        //                ballX--;
+        //            }
+        //        }
+
+        //        //Check which key has been pressed
+        //        switch (Console.ReadKey().Key)
+        //        {
+        //            case ConsoleKey.UpArrow:
+        //                if (rightRacketHeight > 0)
+        //                {
+        //                    rightRacketHeight--;
+        //                }
+        //                break;
+
+        //            case ConsoleKey.DownArrow:
+        //                if (rightRacketHeight < fieldWidth - racketLength - 1)
+        //                {
+        //                    rightRacketHeight++;
+        //                }
+        //                break;
+
+        //            case ConsoleKey.W:
+        //                if (leftRacketHeight > 0)
+        //                {
+        //                    leftRacketHeight--;
+        //                }
+        //                break;
+
+        //            case ConsoleKey.S:
+        //                if (leftRacketHeight < fieldWidth - racketLength - 1)
+        //                {
+        //                    leftRacketHeight++;
+        //                }
+        //                break;
+        //        }
+
+        //        //Clear the rackets’ previous positions
+        //        for (int i = 1; i < fieldWidth; i++)
+        //        {
+        //            Console.SetCursorPosition(0, i);
+        //            Console.WriteLine(" ");
+        //            Console.SetCursorPosition(fieldLength - 1, i);
+        //            Console.WriteLine(" ");
+        //        }
+                
+        //        if (ballY == 1 || ballY == fieldWidth - 1)
+        //        {
+        //            isBallGoingDown = !isBallGoingDown; //Change direction
+        //        }
+
+        //        if (ballX == 1)
+        //        {
+        //            //Left racket hits the ball and it bounces
+        //            if (ballY >= leftRacketHeight + 1 && ballY <= leftRacketHeight + racketLength)
+        //            {
+        //                isBallGoingRight = !isBallGoingRight;
+        //            }
+        //            else //Ball goes out of the field; Right player scores
+        //            {
+        //                rightPlayerPoints++;
+        //                ballY = fieldWidth / 2;
+        //                ballX = fieldLength / 2;
+        //                Console.SetCursorPosition(scoreboardX, scoreboardY);
+        //                Console.WriteLine($"{leftPlayerPoints} | {rightPlayerPoints}");
+        //                if (rightPlayerPoints == 10)
+        //                {
+        //                    goto outer;
+        //                }
+        //            }
+        //        }
+
+        //        if (ballX == fieldLength - 2)
+        //        {
+        //            //Right racket hits the ball and it bounces
+        //            if (ballY >= rightRacketHeight + 1 && ballY <= rightRacketHeight + racketLength)
+        //            {
+        //                isBallGoingRight = !isBallGoingRight;
+        //            }
+        //            else //Ball goes out of the field; Left player scores
+        //            {
+        //                leftPlayerPoints++;
+        //                ballY = fieldWidth / 2;
+        //                ballX = fieldLength / 2;
+        //                Console.SetCursorPosition(scoreboardX, scoreboardY);
+        //                Console.WriteLine($"{leftPlayerPoints} | {rightPlayerPoints}");
+        //                if (leftPlayerPoints == 10)
+        //                {
+        //                    goto outer;
+        //                }
+
+        //            }
+        //        }
+        //        //Left Player
+        //        leftPlayerPoints++;
+        //        ballY = fieldWidth / 2;
+        //        ballX = fieldLength / 2;
+        //        Console.SetCursorPosition(scoreboardX, scoreboardY);
+        //        Console.WriteLine("${leftPlayerPoints} | {rightPlayerPoints}");
+
+        //        if (leftPlayerPoints == 10)
+        //        {
+        //            goto outer;
+        //        }
+
+        //        //Right Player
+        //        rightPlayerPoints++;
+        //        ballY = fieldWidth / 2;
+        //        ballX = fieldLength / 2;
+        //        Console.SetCursorPosition(scoreboardX, scoreboardY);
+        //        Console.WriteLine("${leftPlayerPoints} | {rightPlayerPoints}");
+        //        if (rightPlayerPoints == 10)
+        //        {
+        //            goto outer;
+        //        }
+
+        //    outer:
+        //        Console.Clear();
+        //        Console.SetCursorPosition(0, 0);
+
+        //        if (rightPlayerPoints == 10)
+        //        {
+        //            Console.WriteLine("Right player won!");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Left player won!");
+        //        }
+
+
+        //    }
+            
+
+
+
+
+
+        //}
+
+    }
+}
